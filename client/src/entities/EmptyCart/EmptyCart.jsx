c4b175d692a66aa60270765a7f4fae87c4a9e91e
mdmatvey <lyoshinmdmatvey@gmail.com>
2023-03-09 00:51:20 +0300

    feat: Add empty cart message

---
+++ client/src/entities/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,17 @@
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { Typography } from '@mui/material';
+import './styles.css'
+
+const EmptyCart = () => {
+    return (  
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <AddBoxIcon 
+                className='addSector'
+                sx={{ fontSize: 200 }} 
+            />
+            <Typography>Вы не отметили ни одного сектора</Typography>
+        </div>
+    );
+}
+ 
+export default EmptyCart;
